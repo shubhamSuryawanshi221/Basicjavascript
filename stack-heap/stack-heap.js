d2f7adf8e32f64bec91a62cdac3c35229210b481
Shubham Suryawanshi <150051910+shubhamSuryawanshi221@users.noreply.github.com>
2024-08-31 10:59:07 +0000

    stack-heap

---
+++ stack-heap/stack-heap.js
@@ -0,0 +1,39 @@
+//stack and heap 
+
+let myname = "shubham"
+
+let anothername = myname
+
+anothername = "suryawanshi"
+
+
+
+
+console.log(myname)
+
+console.log(anothername)
+
+
+
+
+let userOne = {
+
+    email : "shubham@gmail.com",
+
+    upi : "user@xyz"
+
+}
+
+
+
+
+let userTwo = userOne
+
+userTwo.email = "xyz@gmail.com"
+
+
+
+
+console.log(userOne.email)
+
+console.log(userTwo.email)
